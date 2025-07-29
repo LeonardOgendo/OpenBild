d357990d889a4fe099f4df15a561b72a99c1a82b
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-07-29 10:38:14 +0300

    Initial commit - setup complete

---
+++ backend/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Article
+from .serializers import ArticleSerializer
+
+# Create your views here.
+
+class ArticleList(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
