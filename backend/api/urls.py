aec71d5a2ae9fa436f3a90936c8baab6f63b33f2
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-07-29 22:29:12 +0300

    Clean backend folder structure

--- backend/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ArticleList
-
-urlpatterns = [
-    path('articles/', ArticleList.as_view(), name="article-list")
-]
--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for api_tests project.
+WSGI config for openCollab project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
