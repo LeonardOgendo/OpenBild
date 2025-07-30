9316fc0e27139f54a2ee22cc32ee0ea5929f0320
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-07-30 22:57:02 +0300

    Connect PostgreSQL

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -58,15 +58,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # Add PostgreSQL or other database configuration here 
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST", "localhost"),
+        'PORT': os.getenv("DB_PORT", "5432"),
+    }
 }
 
 
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
