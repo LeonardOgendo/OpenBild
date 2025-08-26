275a39b26c9756eeb7c27ed0b1d143ef72e1770e
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-08-26 11:50:09 +0300

    Add LogoutView

--- backend/apps/accounts/api/views.py
+++ backend/apps/accounts/api/views.py
@@ -1,21 +1,56 @@
-from rest_framework.generics import CreateAPIView
-from rest_framework import status
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import RegisterSerializer
-from .serializers import CustomTokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegisterSerializer, CustomTokenObtainPairSerializer
 
-class RegisterView(CreateAPIView):
+# Register user view
+class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [permissions.AllowAny]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        user = serializer.save()
 
+        return Response(
+            {
+                "success": True,
+                "message": "User registered successfully",
+                "data": {
+                    "id": user.id,
+                    "email": user.email,
+                    "username": user.username,
+                },
+            },
+            status=status.HTTP_201_CREATED
+        )
 
+# Login View
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+# Logout view
+class LogoutView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        refresh_token = request.data.get("refresh")from rest_framework import status, generics, permissions
+        from rest_framework.response import Response
+        from rest_framework.permissions import AllowAny
+        from rest_framework_simplejwt.views import TokenObtainPairView
+        from rest_framework_simplejwt.tokens import RefreshToken
+        from .serializers import RegisterSerializer, CustomTokenObtainPairSerializer
+        
+        if not refresh_token:
+            return Response({ "detail": "Refresh token is required" }, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({ "detail": "Logout successfull." }, status=status.HTTP_205_RESET_CONTENT)
+        except Exception:
+            # internal errors hidden to clients
+            return Response({ "detail": "Invalid refresh token." }, status=status.HTTP_400_BAD_REQUEST)
+        
