8f6f5de6f79b49a7b4601a1711ffdbbefd39a703
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-08-26 11:48:22 +0300

    Update: Add extra user info to response, upon successful login

--- backend/apps/accounts/api/serializers.py
+++ backend/apps/accounts/api/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 from apps.accounts.models import CustomUser
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
 
     class Meta:
         model = CustomUser
-        fields = ['email', 'username', 'password']
+        fields = ['id', 'email', 'username', 'password']
     
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
@@ -22,4 +23,15 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         # Replace 'username' with 'email'
         attrs['username'] = attrs.get('email', '')
-        return super().validate(attrs)
+        data = super().validate(attrs)
+
+        # add extra user info
+        data.update({
+            "user": {
+                "id": self.user.id,
+                "email": self.user.email,
+                "username": self.user.username,
+            }
+        })
+
+        return data
